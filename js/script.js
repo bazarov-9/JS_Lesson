7ba4cb6c2aab674012eda8f0798cb15096eea67c
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-11-05 15:07:33 +0500

    dasdasdasd

--- js/script.js
+++ js/script.js
@@ -26,3 +26,78 @@
 // a=16;//hosting
 // let a=15;
 // console.log(a);
+
+// const birthDate=2003;
+// birthDate=2006;
+// console.log(birthDate);
+
+// const a=;
+// console.log(typeof a);
+
+// let a = 5;
+// let b = 3;
+// let sum = a + b; // 8
+// let difference = a - b; // 2
+// let product = a * b; // 15
+// let quotient = a / b; // 1.66667
+// let remainder = a % b; // 2
+// let power = a ** b; // 125
+// console.log(sum, difference, product, quotient, remainder, power);
+// console.log(15%4);
+
+// console.log(5 == '5'); //true
+// console.log("5" === '15');//false
+// console.log(5 != '5'); //false
+// console.log(5 !== '5');//true
+// console.log(5 > 3); 
+// console.log(5 < 3); 
+
+// let a=true;
+// console.log(typeof !a);
+
+// let a = false;
+// let b = false;
+// console.log(a && b); 
+// console.log(a || b); 
+// console.log(!a); 
+
+
+// console.log(5 & 3); // 1
+// console.log(5 | 3); // 7
+// console.log(5 ^ 3); // 6
+// console.log(~5); // -6
+// console.log(6 << 1); // 10
+// console.log(5 >> 2); // 2
+
+// let a=5;
+// console.log(a+=1);
+// let a = 5;
+// a += 3; // 8 a=a+3;
+// a -= 2; // 6
+// a *= 2; // 12
+// a /= 4; // 3
+// // a %= 2; // 1
+// console.log(a);
+
+
+// const a = 3; // Same as 4 ** (3 ** 2); evaluates 
+// const b = 4; // Same as (4 / 3) / 2; evaluates to]\
+// // console.log(typeof(typeof (typeof((typeof (typeof a)+b))+typeof b)>5));
+// let b="hello";
+// console.log((b += ' world'));
+
+
+// let baz = true;
+// baz += 1; // 2
+// let b=(true += false); // 2
+// console.log(b);
+
+// let a=false;
+// console.log(a);
+// let x = 5;
+// let y = 10;
+// let z = 25;
+// x = y;  
+// x= (y = z); 
+// console.log(x, y, z);
+
