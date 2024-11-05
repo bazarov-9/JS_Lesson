d35d0b3834ddc2998b4f61c5a3da114ec6e04981
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-11-05 17:03:01 +0500

    zsczxczxc

--- js/script.js
+++ js/script.js
@@ -100,4 +100,94 @@
 // x = y;  
 // x= (y = z); 
 // console.log(x, y, z);
+// 4-dars homework yechim
+// const athamMass=78;
+// const athamHeight=1.69;
+// const sardorMass=78;
+// const sardorHeight=1.69;
+
+// const adhamBMI=(athamMass/athamHeight**2);
+// const sardorBMI=(sardorMass/sardorHeight**2);
+
+// const markHigherBMI =(adhamBMI>sardorBMI);
+// console.log(markHigherBMI);
+
+// let ism = "Sardorbek";
+// let salomlashish = "Assalamu alaykum , " + ism + "   !";
+// console.log(salomlashish);
+
+// let name1="Temurbek",
+// name2="ishingni tez bitir";
+// console.log(name1+" "+name2);
+
+
+// let ism = "Sardorbek";
+// let yosh = 25;
+// let salomlashish = `Asslamu alaykum, mening ismim ${ism} va men ${yosh} yoshdaman.`;
+// console.log(salomlashish);
+
+// let name1="Temurbek";
+// let name2="ishingni tez bitir";
+// console.log(`Assalamu o alaykum sening isming ${name1}`);
+
+// let o='O\''
+// console.log(o);
+
+// let string="qanday guzal bu \nolam"
+// console.log(string);
+
+// let string = `qanday guzal bu  olam`
+// console.log(string);
+
+// Oddiy string bilan
+// let oddiyMatn = "Bu birinchi qatordir.\nBu ikkinchi qatordir.";
+// console.log(oddiyMatn);
+// // Template literal bilan
+// let koPqatOrliMatn = `Bu birinchi qatordir.
+// Bu ikkinchi qatordir.`;
+// console.log(koPqatOrliMatn);
+
+// let a = 10;
+// let b = 5;
+// console.log(`10 : 5 = ${a / b}`);
+
+// let ism = "Sardorbek";
+// let fan = "JavaScript";
+// let vaqt = 3;
+
+// let xabar = `Assalamu alaykuum, ${ism}! Siz ${fan} fanini ${vaqt} oydan beri o‘rganmoqdasiz. Zo‘r ish!`;
+// console.log(xabar);
+
+// Natija: Salom, Sardorbek! Siz JavaScript fanini 3 oydan beri o‘rganmoqdasiz. Zo‘r ish!
+
+// let yosh = prompt("Yoshingizni kiriting");
+
+// if (yosh >= 18) {
+//   console.log("Sizga ruxsat beriladi.");  // Shart rost bo'lgani uchun bu ishga tushadi.
+// }else {
+//   console.log("Kirish taqiqlanadi.");
+// }
+// let yosh2 = 16;
+// let xabar = yosh2 >= 18 ? "Kirish ruxsat" : "Kirish taqiqlangan";
+// console.log(xabar);  // Natija: Kirish taqiqlangan
+// let baho = 85;
+
+// if (baho >= 90) {
+//   console.log("A'lo baho");
+// } else if (baho >= 70) {
+//   console.log("Yaxshi baho");
+// } else {
+//   console.log("Yana urinib ko'ring");
+// }
+// let age=8;
+// let natija = age>10 ? "Shart rost" : "Shart yolg'on";
+// console.log(natija);
+
+let data=prompt("Havo haroratini kiriting");
+
+if(0<data<10){
+    alert(`Siz kiritgan son ${data} toq son`)
+}else{
+    alert(`Siz kiritgan son ${data} juft son`)
+}
 
